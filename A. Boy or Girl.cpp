fde6492071910736949e1e71277b5ad643759602
Tanvirul Islam <58099621+tanveer-610@users.noreply.github.com>
2023-03-04 20:36:28 +0600

    Add files via upload

---
+++ A. Boy or Girl.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    string s;
+    int c=0;
+    cin>>s;
+    sort(s.begin(),s.end());
+
+    for(int i=0; i<s.size(); i++)
+    {
+        if(s[i]!= s[i+1])
+        {
+            c++;
+        }
+    }
+
+    if(c%2==0){
+        cout<<"CHAT WITH HER!"<<endl;
+    }else{
+        cout<<"IGNORE HIM!"<<endl;
+    }
+    return 0;
+}
