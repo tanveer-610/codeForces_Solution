0e1af44a9c796c61675ae8c472119888ea802b3e
Tanvirul Islam <58099621+tanveer-610@users.noreply.github.com>
2023-03-24 14:14:42 +0600

    Create 486A Calculating Function.cpp

---
+++ 486A Calculating Function.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    long long int n, d;
+    cin >> n;
+    if(n%2 == 0)
+    {
+        cout << n / 2;
+    }
+    else
+    {
+        d = -(n / 2 + 1);
+        cout << d;
+    }
+}
